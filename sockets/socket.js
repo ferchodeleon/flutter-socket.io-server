6ae631d4e818b92371d9e28cd46705530dabfd56
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-06 16:43:08 -0500

    add function of delete band

--- sockets/socket.js
+++ sockets/socket.js
@@ -25,14 +25,28 @@ io.on("connection", (client) => {
     io.emit("Mensaje", { admin: "Nuevo mensaje" });
   });
 
-  client.on('vote-band', (payload) => {
-    bands.voteBand(payload.id);
-    io.emit('active-bands', bands.getBands());
-  });
-
+  
   client.on("new-message", (payload) => {
     console.log("new message", payload);
     io.emit("new-message", payload); // En este caso lo emite a todos
     client.broadcast.emit("new-message", payload); // Aqui solo lo emite a todos menos al que lo emitio
   });
+  
+  client.on('vote-band', (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  });
+  
+  client.on("add-band", (payload) => {
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit('active-bands', bands.getBands());
+  });
+
+  
+  client.on("delete-band", (payload) => {
+    console.log(payload);
+    bands.deletedBand(payload.id);
+    io.emit('active-bands', bands.getBands());
+  })
 });
