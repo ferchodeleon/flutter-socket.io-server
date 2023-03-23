aab379f5f42e82b2f9328d39ea8136ef040c2673
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-23 15:00:10 -0500

    refactor code and add validation of fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,6 @@
 const {response} = require('express');
-const { validationResult } = require('express-validator');
 
-const createUser = (req, res = response) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-    
+const createUser = (req, res = response) => {    
     res.json({
         ok: true,
         msj: 'Crear usuario!'
--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,16 @@ path: '/api/login'
 const  {Router, response} = require('express');
 const { check } = require('express-validator');
 const { createUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 router.post('/new', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El campo email es obligatorio').not().isEmpty(),
+    check('email', 'El correo ingresado no es un correo valido').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validateFields
 ], createUser);
 
 module.exports = router;
