b4f23247241fdb5adc825c94c2785aa69a7d3200
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-27 10:16:26 -0500

    validate email not exist

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,14 +4,32 @@ const User = require('../models/users');
 
 const createUser = async (req, res = response) => {  
     
-    const user = new User(req.body)
-    
-    await user.save();
+    const {email} = req.body;
 
-    res.json({
-        ok: true,
-        body: req.body
-    })
+    try {
+        const existeEmail = await User.findOne({email});
+        if(existeEmail){
+            return res.status(400).json({
+                ok:false,
+                msg: 'El correo ya está registrado'
+            })
+        }
+        
+        const user = new User(req.body);
+        await user.save();
+        res.json({
+            ok: true,
+            user
+        })
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+    
+    
 }
 
 module.exports = {
